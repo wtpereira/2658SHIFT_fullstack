5b83ce5078df1e437e598e70de48dfdc997923ba
Wellington <wtpereira@gmail.com>
2024-10-11 10:08:00 -0300

    Aprimorando o código.

--- gestao/urls.py
+++ gestao/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'gestao'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cadastrar/', views.cadastrar, name='cadastrar'),
-    path('listar/', views.listar, name='listar'),
+    # path('cadastrar/', views.cadastrar, name='cadastrar'),
+    # path('salvar/', views.save, name='save'),
+    path('salvar/', views.CadastrarView.as_view(), name='save'),
+    # path('listar/', views.listar, name='listar'),
+    path('listar/', views.ListarView.as_view(), name='listar'),
+    # path('livro/<int:livro_id>', views.view, name='view'),
+    path('livro/<int:pk>', views.DetailView.as_view(), name='view'),
+    path('teste/', views.teste, name='teste'),
 ]
--- gestao/views.py
+++ gestao/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, HttpResponseRedirect, render
+from django.urls import reverse_lazy
+from django.views import generic
 from .models import Livro
 
 def index(request):
     return render(request, 'gestao/pagina.html')
 
-def cadastrar(request):
-    return render(request, 'gestao/form.html')
+class CadastrarView(generic.CreateView):
+    model = Livro
+    template_name = 'gestao/form.html'
+    success_url = reverse_lazy('gestao:index')
+    fields = ['titulo', 'paginas']
 
-def listar(request):
-    livros = Livro.objects.all()
-    return render(request, 'gestao/list.html', {'livros': livros})
+# def cadastrar(request):
+#     return render(request, 'gestao/form.html')
+
+# def save(request):
+#     titulo = request.POST.get('titulo')
+#     paginas = int(request.POST.get('paginas'))
+#     livro = Livro(titulo=titulo, paginas=paginas)
+#     livro.save()
+#     return HttpResponseRedirect(reverse('gestao:index'))
+
+class ListarView(generic.ListView):
+    template_name = 'gestao/list.html'
+    context_object_name = 'livros'
+
+    def get_queryset(self):
+        return Livro.objects.all()
+
+# def listar(request):
+#     livros = Livro.objects.all()
+#     return render(request, 'gestao/list.html', {'livros': livros})
+
+
+class DetailView(generic.DetailView):
+    model = Livro
+    template_name = 'gestao/view.html'
+
+
+# def view(request, livro_id):
+#     # livro = Livro.objects.get(id=livro_id)
+#     livro = get_object_or_404(Livro, pk=livro_id)
+#     return render(request, 'gestao/view.html', { 'livro': livro })
+
+def teste(request):
+    return render(request, 'gestao/base_gestao.html')
